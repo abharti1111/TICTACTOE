05da45f5da3dbc61e3874508db5e68acec0666aa
Avinash Bharti <abharti1111@gmail.com>
2020-12-23 00:02:33 +0530

    fixed critical bugs 2

--- mylogics.js
+++ mylogics.js
@@ -14,42 +14,40 @@ function gameCompleteLogic() {
     let myArr = []
     let idName = "cell"
     let k = 0
-    for(i=0;i<3;i++){
-        for(j=0;j<3;j++){
-            var val = document.getElementById(idName.concat(i.toString(),j.toString())).innerHTML
+    for( let i=0;i<3;i++){
+        for(let j=0;j<3;j++){
+            let val = document.getElementById(idName.concat(i.toString(),j.toString())).innerHTML
             myArr.push((val==="")?k++:val)
         }
-        
     }
-    console.log(myArr);
-    winningConditions.forEach(element => {
-        if(myArr[element[0]] === myArr[element[1]] && myArr[element[0]] === myArr[element[2]] ){
-            console.log("executed");
-            // gameOver = 1
-            document.getElementById("result").innerHTML = turn===0?"player 2 winner":"player 1 winner"
-            return ;
+
+    for (let i = 0;i<winningConditions.length; i++) {
+        // console.log(winningConditions[i]);
+        if(myArr[winningConditions[i][0]] === myArr[winningConditions[i][1]] && myArr[winningConditions[i][0]] === myArr[winningConditions[i][2]] ){
+            gameOver = 1;
+            break;
         }else{
-            // document.getElementById("result").innerHTML = "Oop's its a Tie"
-            // console.log(k);
-            gameOver = gameOver===0?0:-1
-            return;
+            gameOver = k===0?-1:0
         }
-    })
+    }
 
 }
 
 document.getElementById("grid").addEventListener('click',function(e){
-    // console.log(e);
-    if(gameOver === 0 ){
+    // console.log("clicked ", gameOver);
+    if(gameOver === 0){
         e.target.innerHTML = turn===0?"X":"O"
         turn = 1-turn
         gameCompleteLogic()
         if(gameOver === -1){
-            document.getElementById("result").innerHTML = "Oop's its a Tie"
-            // alert(turn===0?"player 2 winner":"player 1 winner")
+            document.getElementById("result").innerHTML = "Oop's Its A Tie"
+        }else if(gameOver === 1){
+            console.log(gameOver);
+            document.getElementById("result").innerHTML = turn===0?"player 2 winner":"player 1 winner"
         }
-
     }
+    
+    
 })
 
 
